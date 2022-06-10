4f7fe5476c63b2d1f88cfbdd271a0dabd7ac509c
theanxiuser <bishalpoudel607@gmail.com>
2022-06-10 21:44:44 +0545

    client to access server using socket

---
+++ fun_with_python/client1.py
@@ -0,0 +1,16 @@
+# A very simple web client
+import socket
+
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(("127.0.0.1", 9000))
+cmd = "GET http://127.0.0.1/romeo.txt HTTP/1.1\r\n\r\n".encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end="")
+
+mysock.close()
