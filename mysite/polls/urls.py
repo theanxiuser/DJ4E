abde7cdc86dbc299c64f21d20299792feab8787f
theanxiuser <bishalpoudel607@gmail.com>
2022-06-13 17:01:27 +0545

    get_object_or_404

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 # defining url path for polls app
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:question_id>", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
