09feb3475f9a1c3b1a6ae1b2c1a036acc8acde63
theanxiuser <bishalpoudel607@gmail.com>
2022-06-15 17:53:30 +0545

    set cookie in hello app

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
+
 def helloview(request):
     visitor_num = request.session.get("visitor_num", 0) + 1
     request.session["visitor_num"] = visitor_num
     if visitor_num > 4:
         del(request.session["visitor_num"])
-    response = HttpResponse("view count = " + str(visitor_num))
-    return response
+    resp = HttpResponse("view count=" + str(visitor_num))
+    resp.set_cookie('dj4e_cookie', '7aef8f65', max_age=1000)
+    return resp
