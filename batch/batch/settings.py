fc52932cb5304101d00f024d411aa3fd4ab51050
theanxiuser <bishalpoudel607@gmail.com>
2022-06-18 08:30:23 +0545

    unesco app added to batch setting

--- batch/batch/settings.py
+++ batch/batch/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     "django_extensions",
+    "unesco.apps.UnescoConfig"
 ]
 
 MIDDLEWARE = [
