e9bd2b5cfb1aee1599fcaa89914a2f60b29dec3a
theanxiuser <bishalpoudel607@gmail.com>
2022-06-18 09:12:24 +0545

    unesco load scripte done

--- batch/scripts/unesco_load.py
+++ batch/scripts/unesco_load.py
@@ -10,23 +10,29 @@ def run():
     reader = csv.reader(fhand)
     next(reader)  # Advance past the header
 
-    Person.objects.all().delete()
-    Course.objects.all().delete()
-    Membership.objects.all().delete()
+    Category.objects.all().delete()
+    State.objects.all().delete()
+    Region.objects.all().delete()
+    ISO.objects.all().delete()
+    Site.objects.all().delete()
 
     # Format
-    # email,role,course
-    # jane@tsugi.org,I,Python
-    # ed@tsugi.org,L,Python
+    # name, description, justification, year, longitude, latitude,
+    # area_hectares, category, state, region, iso
 
     for row in reader:
         print(row)
 
-        p, created = Person.objects.get_or_create(email=row[0])
-        c, created = Course.objects.get_or_create(title=row[2])
+        c, created = Category.objects.get_or_create(name=row[7])
+        s, created = State.objects.get_or_create(name=row[8])
+        r, created = Region.objects.get_or_create(name=row[9])
+        i, created = ISO.objects.get_or_create(name=row[10])
 
-        r = Membership.LEARNER
-        if row[1] == 'I':
-            r = Membership.INSTRUCTOR
-        m = Membership(role=r, person=p, course=c)
-        m.save()
+        try:
+            y = int(row[3])
+        except:
+            y = None
+
+        site = Site(name=row[0], description=row[1], justification=row[2], year=y, longitude=row[4], latitude=row[5], area_hectares=row[6], category=c, state=s, region=r, iso=i)
+
+        site.save()
