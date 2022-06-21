4fd82b3acd5c5fac79c64b9cb3f9c1857d76b5af
theanxiuser <bishalpoudel607@gmail.com>
2022-06-21 19:55:00 +0545

    urls for breed

--- mysite/cats/urls.py
+++ mysite/cats/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import MainView, CreateCats, UpdateCats, DeleteCats
+from . views import MainView, CreateCats, UpdateCats, DeleteCats, ShowBreed, CreateBreed, UpdateBreed, DeleteBreed
 
 app_name = "cats"
 urlpatterns = [
@@ -7,4 +7,8 @@ urlpatterns = [
     path("main/create/", CreateCats.as_view(), name="create_cats"),
     path("main/<int:pk>/update/", UpdateCats.as_view(), name="update_cats"),
     path("main/<int:pk>/delete/", DeleteCats.as_view(), name="delete_cats"),
+    path("breed/", ShowBreed.as_view(), name="breed_list"),
+    path("breed/create/", CreateBreed.as_view(), name="create_breed"),
+    path("breed/<int:pk>/update/", UpdateBreed.as_view(), name="update_breed"),
+    path("breed/<int:pk>/delete/", DeleteBreed.as_view(), name="delete_breed"),
 ]
