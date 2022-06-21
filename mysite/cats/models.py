7ee4f806b453fbb2b82468940d29f0d9d6e35403
theanxiuser <bishalpoudel607@gmail.com>
2022-06-21 17:38:57 +0545

    model for cat

--- mysite/cats/models.py
+++ mysite/cats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# breed class
+class Breed(models.Model):
+    name = models.CharField(max_length=200)
+
+# cat class
+class Cat(models.Model):
+    nickname = models.CharField(max_length=200)
+    weight = models.FloatField()
+    breed = models.ForeignKey("Breed", on_delete=models.CASCADE, null=False)
